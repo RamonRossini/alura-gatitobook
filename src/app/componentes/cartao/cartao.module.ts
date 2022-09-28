2b301759b92100cd1832ae0fd9684db2c9533397
RamonRossini <ramonhbonfim@gmail.com>
2022-09-27 22:55:39 -0300

    Aula 1 parte 2

---
+++ src/app/componentes/cartao/cartao.module.ts
@@ -0,0 +1,16 @@
+import { CartaoComponent } from './cartao.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    CartaoComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CartaoComponent
+  ]
+})
+export class CartaoModule { }
