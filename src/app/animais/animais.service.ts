c3d729f3b512fc41d9a477275c7cfced28c97c89
RamonRossini <ramonhbonfim@gmail.com>
2022-10-04 00:20:40 -0300

    Aula 3 parte 2

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,11 +1,12 @@
 import { TokenService } from './../autenticacao/token.service';
-import { Animais } from './animais';
-import { Observable } from 'rxjs';
+import { Animais, Animal } from './animais';
+import { catchError, map, mapTo, Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 const API = environment.apiURL;
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root'
@@ -15,14 +16,28 @@ export class AnimaisService {
   constructor(
     private http: HttpClient,
     private tokenService: TokenService
-    ) { }
+  ) { }
 
   listaDoUsuario(nomeDoUsuario: string): Observable<Animais> {
-    const token = this.tokenService.retornarToken();
-    const headers = new HttpHeaders().append('x-access-token', token);
-    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
-      headers
-    })
+    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`);
+  }
+
+  buscarPorId(id: number): Observable<Animal> {
+    return this.http.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  excluirAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtirAnimal(id: number): Observable<any> {
+    return this.http.post(`${API}/photos/${id}/like`, {}, { observe: 'response' })
+    .pipe(
+      mapTo(true),
+      catchError((error) => {
+        return error.status === NOT_MODIFIED ? of(false) : throwError(() => new Error('test'));
+      })
+    )
   }
 
 }
